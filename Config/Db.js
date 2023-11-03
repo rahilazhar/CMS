2962243e0c4c53c2234ca2f2187f74658258eb05
Rahil Azhar <rahil.azhar10.com>
2023-11-03 15:26:49 +0500

    first commit

--- Config/Db.js
+++ Config/Db.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose')
 const colors = require('colors')
 
 
-
-const Connectionstring = process.env.MONGO_URL
-
 const dbconnection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://rahilazhar10:rahilazhar@cluster0.uijwffp.mongodb.net/CMSS?retryWrites=true&w=majority")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('Database is Connected'.bgBlue)
     } catch (error) {
         console.log(`Error in Database connection ${error}`.bgRed)
